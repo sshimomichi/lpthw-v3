9950c61b4e755e6bd8f59374c9de55b5427cd8c9
Sebastian Shimomichi <onacoffeebreaknow@gmail.com>
2021-02-20 13:20:10 +0800

    Exercise 1

---
+++ ex1.py
@@ -0,0 +1,14 @@
+print("Hello world!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd rather you 'not'.")
+print('I "said" do not touch this.')
+
+# Study Drill
+
+print("This whole line is a string.")
+# print("This won't print")
+print("you can 'have' single quotations in a string.")
+print("'"'"'"'uWu'"'"'"'")
